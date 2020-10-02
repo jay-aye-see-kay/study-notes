dc76ce255fb04292969663b00958934a4ff7f152
Jack Rose <user@jackrose.co.nz>
2020-10-02 12:59:36 +1000

    print frontpage to terminal

--- rust-hacks/reddit-get/src/lib.rs
+++ rust-hacks/reddit-get/src/lib.rs
@@ -1,40 +1,52 @@
-use serde::Deserialize;
 use reqwest::blocking;
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct RResponse {
-    pub kind: String,
+    kind: String,
     pub data: RData,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct RData {
-    pub dist: i32,
+    dist: i32,
     pub children: Vec<RPost>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct RPost {
-    pub kind: String,
-    pub data: RPostData,
+    kind: String,
+    data: RPostData,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct RPostData {
-    pub subreddit: String,
-    pub title: String,
-    pub permalink: String,
-    pub author: String,
-    pub domain: String,
-    pub url: String,
-    pub created: f64,
-    pub created_utc: f64,
-    pub ups: i32,
-    pub downs: i32,
-    pub score: i32,
-    pub num_comments: i32,
+struct RPostData {
+    subreddit: String,
+    title: String,
+    permalink: String,
+    author: String,
+    domain: String,
+    url: String,
+    created: f64,
+    created_utc: f64,
+    ups: i32,
+    downs: i32,
+    score: i32,
+    num_comments: i32,
 }
 
+impl std::fmt::Display for RPost {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{title} (from: /r/{sub} /u/{user})\n{url}",
+            title = &self.data.title,
+            sub = &self.data.subreddit,
+            user = &self.data.author,
+            url = &self.data.url
+        )
+    }
+}
 
 pub fn get_sub(sub: Option<&str>) -> Result<RResponse, Box<dyn std::error::Error>> {
     let sub = match sub {
--- rust-hacks/reddit-get/src/main.rs
+++ rust-hacks/reddit-get/src/main.rs
@@ -3,7 +3,9 @@ use reddit_get::get_sub;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let body = get_sub(None)?;
 
-    let first_post = body.data.children.first().unwrap();
-    println!("first_post: {:?}", first_post);
+    for (i, post) in body.data.children.iter().enumerate() {
+        println!("{}: {}\n", i + 1, post);
+    }
+
     Ok(())
 }
